f3db24e57fe03056c1d1a2b21cdc4cd29acce757
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-11-18 04:27:12 +0900

    トップの画像を追加

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,10 @@
 import Typography from '@mui/material/Typography';
+import Image from "./headerIcon.jpg";
 
 const Home = () => {
   return (
     <>
+      <img src={Image} width="100%" alt="トップアイコン" />
       <Typography paragraph>
         大阪府立大学、大阪市立大学が合併し大阪公立大学となりました。
       </Typography>
