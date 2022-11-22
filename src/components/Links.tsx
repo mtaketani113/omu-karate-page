cbe9dd2f574c976da3872d2775147e3ed428eb36
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-11-22 12:56:02 +0900

    リンク集を修正

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,14 +1,24 @@
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
-
 const Links = () => {
   return (
     <>
-      <a href="https://twitter.com/omu_karatedo?">大阪公立大学のTwitter</a>
-      <TwitterTimelineEmbed
-        sourceType="profile"
-        screenName="omu_karatedo"
-        options={{ height: 600 }}
-      />
+      <h2>Twitter</h2>
+      <li>
+        <a href="https://twitter.com/omu_karatedo">大阪公立大学のTwitter</a>
+      </li>
+      <h2>合併前のホームページ</h2>
+      <li>
+        <a href="http://ocukaratedo.web.fc2.com/">大阪市立大学空手道部のホームページ</a>
+      </li>
+      <li>
+        <a href="https://opu-karatedo-life.jimdofree.com/">大阪府立大学空手道部のホームページ</a>
+      </li>
+      <h2>空手道関連</h2>
+      <li>
+        <a href="https://www.jkf.ne.jp/">全日本空手道連盟</a>
+      </li>
+      <li>
+        <a href="https://www.jukf.org/">全日本学生空手道連盟</a>
+      </li>
     </>
   );
 };
