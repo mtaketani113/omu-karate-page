aac0a48ae4511b47324186286e0c621c21918592
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-11-28 23:55:26 +0900

    shareボタンを修正

--- src/components/parts/ShareButton.tsx
+++ src/components/parts/ShareButton.tsx
@@ -1,43 +1,38 @@
-import { Grid } from "@mui/material";
+import { Grid } from '@mui/material';
 import {
-    FacebookShareButton,
-    LineShareButton,
-    TwitterShareButton,
-    FacebookIcon,
-    LineIcon,
-    TwitterIcon,
-  } from "react-share";
-  
-  const URL = 'https://mtaketani113.github.io/omu-karate-page/';
-  const QUOTE = '大阪公立大学空手道部 応援ページ';
-  
-  const ShareButton = () => {
-    return (
-      <>
-<Grid container spacing={10} style={{paddingTop:'15px'}}>
-  <Grid item xs={1}>
-  <FacebookShareButton url={URL} quote={QUOTE}>
-          <FacebookIcon size={48} round />
-        </FacebookShareButton>
-  </Grid>
-  <Grid item xs={1}>
-  <TwitterShareButton url={URL} title={QUOTE}>
-          <TwitterIcon size={48} round />
-        </TwitterShareButton>
-  </Grid>
-  <Grid item xs={1}>
-  <LineShareButton url={URL} title={QUOTE} >
-          <LineIcon size={48} round />
-        </LineShareButton>
-  </Grid>
-  </Grid>
+  FacebookShareButton,
+  LineShareButton,
+  TwitterShareButton,
+  FacebookIcon,
+  LineIcon,
+  TwitterIcon,
+} from 'react-share';
 
+const URL = 'https://mtaketani113.github.io/omu-karate-page/';
+const QUOTE = '大阪公立大学空手道部 応援ページ';
 
-
-
-
-      </>
-    )
-  }
+const ShareButton = () => {
+  return (
+    <>
+      <Grid container spacing={0} style={{ paddingTop: '15px' }}>
+        <Grid item xs={2}>
+          <FacebookShareButton url={URL} quote={QUOTE}>
+            <FacebookIcon size={48} round />
+          </FacebookShareButton>
+        </Grid>
+        <Grid item xs={2}>
+          <TwitterShareButton url={URL} title={QUOTE}>
+            <TwitterIcon size={48} round />
+          </TwitterShareButton>
+        </Grid>
+        <Grid item xs={2}>
+          <LineShareButton url={URL} title={QUOTE}>
+            <LineIcon size={48} round />
+          </LineShareButton>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default ShareButton;
