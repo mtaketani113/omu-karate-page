920a4651dd4f8a620a52dacb90237b599cefe5b5
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-12-02 01:08:25 +0900

    SNSリンクのURL修正

--- src/components/parts/ShareButton.tsx
+++ src/components/parts/ShareButton.tsx
@@ -7,26 +7,30 @@ import {
   LineIcon,
   TwitterIcon,
 } from 'react-share';
+import { useLocation } from "react-router-dom"
 
-const URL = 'https://mtaketani113.github.io/omu-karate-page/';
+const URL = 'https://mtaketani113.github.io/omu-karate-page';
 const QUOTE = '大阪公立大学空手道部 応援ページ';
 
 const ShareButton = () => {
+
+  const location = useLocation();
+
   return (
     <>
       <Grid container spacing={0} style={{ paddingTop: '15px' }}>
         <Grid item xs={2}>
-          <FacebookShareButton url={URL} quote={QUOTE}>
+          <FacebookShareButton url={URL + location.pathname} quote={QUOTE}>
             <FacebookIcon size={48} round />
           </FacebookShareButton>
         </Grid>
         <Grid item xs={2}>
-          <TwitterShareButton url={URL} title={QUOTE}>
+          <TwitterShareButton url={URL + location.pathname} title={QUOTE}>
             <TwitterIcon size={48} round />
           </TwitterShareButton>
         </Grid>
         <Grid item xs={2}>
-          <LineShareButton url={URL} title={QUOTE}>
+          <LineShareButton url={URL + location.pathname} title={QUOTE}>
             <LineIcon size={48} round />
           </LineShareButton>
         </Grid>
