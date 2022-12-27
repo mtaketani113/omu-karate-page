1ba59ca11c0468593e5b8b0034d931e0d4d21726
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2022-12-28 01:30:35 +0900

    画像、動画のサイズを調整

--- src/components/Place.tsx
+++ src/components/Place.tsx
@@ -1,8 +1,11 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useMedia } from 'react-use';
 
 const Place = () => {
+  const isWide = useMedia('(min-width: 780px)');
+
   return (
     <>
       <HelmetProvider>
@@ -14,30 +17,36 @@ const Place = () => {
       <h3 className="anchor" id="sugimoto">
         杉本キャンパス 新武道場
       </h3>
-      <iframe
-        title="新武道場"
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d976.4800980389026!2d135.50802317627867!3d34.59216561593446!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000dc1300eaceed%3A0xaadb891609ab1fc4!2z44CSNTU4LTg1ODUg5aSn6Ziq5bqc5aSn6Ziq5biC5L2P5ZCJ5Yy65p2J5pys77yT5LiB55uu77yT4oiS77yR77yT77yYIOaWsOatpumBk-WgtA!5e0!3m2!1sja!2sjp!4v1668716020197!5m2!1sja!2sjp"
-        style={{ border: '0', width: '100%', aspectRatio: '16/9' }}
-        allowFullScreen={true}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <iframe
+          title="新武道場"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d976.4800980389026!2d135.50802317627867!3d34.59216561593446!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000dc1300eaceed%3A0xaadb891609ab1fc4!2z44CSNTU4LTg1ODUg5aSn6Ziq5bqc5aSn6Ziq5biC5L2P5ZCJ5Yy65p2J5pys77yT5LiB55uu77yT4oiS77yR77yT77yYIOaWsOatpumBk-WgtA!5e0!3m2!1sja!2sjp!4v1668716020197!5m2!1sja!2sjp"
+          style={{ border: '0', width: isWide ? '90%' : '100%', aspectRatio: '16/9' }}
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </Box>
       <h3 className="anchor" id="nakamozu">
         中百舌鳥キャンパス 体育館剣道場
-      </h3>
-      <iframe
-        title="中百舌鳥道場"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d775.4603984145501!2d135.50723243628272!3d34.546303878949594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000dbcc4b8d0ddd%3A0x74010ebd35722f16!2z44CSNTk5LTgyMzEg5aSn6Ziq5bqc5aC65biC5Lit5Yy65a2m5ZyS55S677yRIEMy5qOfICjkvZPogrLppKgp!5e0!3m2!1sja!2sjp!4v1668715461771!5m2!1sja!2sjp"
-        style={{ border: '0', width: '100%', aspectRatio: '16/9' }}
-        allowFullScreen={true}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
-      <img
-        src="https://omu-karate-org.github.io/images/place/nakamozu.jpg"
-        width="100%"
-        alt="トップアイコン"
-      />
+      </h3>{' '}
+      <Box display="flex" justifyContent="center" alignItems="center" marginBottom="10px">
+        <iframe
+          title="中百舌鳥道場"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d775.4603984145501!2d135.50723243628272!3d34.546303878949594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000dbcc4b8d0ddd%3A0x74010ebd35722f16!2z44CSNTk5LTgyMzEg5aSn6Ziq5bqc5aC65biC5Lit5Yy65a2m5ZyS55S677yRIEMy5qOfICjkvZPogrLppKgp!5e0!3m2!1sja!2sjp!4v1668715461771!5m2!1sja!2sjp"
+          style={{ border: '0', width: isWide ? '90%' : '100%', aspectRatio: '16/9' }}
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>{' '}
+      </Box>
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <img
+          src="https://omu-karate-org.github.io/images/place/nakamozu.jpg"
+          width={isWide ? '90%' : '100%'}
+          alt="中百舌鳥"
+        />
+      </Box>
       <h2 className="anchor" id="time">
         練習時間
       </h2>
@@ -46,12 +55,10 @@ const Place = () => {
         <li>水曜日・・・17:00~19:00</li>
         <li>土曜日・・・10:00~12:00</li>
       </Typography>
-
       <h3>中百舌鳥キャンパス</h3>
       <Typography paragraph>
         <li>木曜日・・・17:00~19:00 ※不定期</li>
       </Typography>
-
       <Typography paragraph>
         【重要】不定期のため、<Link to={'/schedule'}>練習予定</Link>をご確認ください
       </Typography>
--- src/components/TrainingMenue.tsx
+++ src/components/TrainingMenue.tsx
@@ -13,8 +13,14 @@ import {
 } from '@mui/material';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import ImageGallery from 'react-image-gallery';
+import { useMedia } from 'react-use';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
+/**
+ * 練習メニュー・風景の画面
+ * 
+ * @returns 
+ */
 const TrainingMenue = () => {
   const { date } = useParams<{ date: string }>();
   const [rows, setRows] = useState<ReactNode>([]);
@@ -22,13 +28,45 @@ const TrainingMenue = () => {
   const [galleryImages, setGrallyImages] = useState<any>([]);
   const [nextPage, setNextPage] = useState<string | null>(null);
   const [prePage, setPrePage] = useState<string | null>(null);
+  const isWide = useMedia('(min-width: 780px)');
 
+  /**
+   * 練習内容のサイズを設定
+   */
   const style = {
-    width: '100%',
+    width: isWide ? '90%' : '100%',
     margin: '0 auto',
     marginTop: 10,
+    marginBottom: 10,
   };
 
+  /**
+   * 動画のuseStateを設定
+   * 
+   * 動画の初期表示とサイズ変更時に利用
+   * @param videos 
+   */
+  const setVideosFunc = (videos: Array<any>) => {
+    let tmpVideos: Array<ReactNode> = [];
+    for (let i = 0; i < videos.length; i++) {
+      let video = videos[i];
+      tmpVideos.push(
+        <Box key={i} display="flex" justifyContent="center" alignItems="center" marginBottom="10px">
+          <iframe
+            style={{ width: isWide ? '80%' : '100%', aspectRatio: '16/9' }}
+            src={video}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </Box>,
+      );
+    }
+    setVideos(tmpVideos);
+  }
+
+  // 初期表示、日付切り替え時に動作
   useEffect(() => {
     const trainings: any = Training;
     const training: any = date != null ? trainings[date] : {};
@@ -58,28 +96,20 @@ const TrainingMenue = () => {
     setGrallyImages(tmpGalleryImages);
 
     const videos: Array<any> = training.videos;
-    let tmpVideos: Array<ReactNode> = [];
-    for (let i = 0; i < videos.length; i++) {
-      let video = videos[i];
-      tmpVideos.push(
-        <div style={{ width: '100%', aspectRatio: '16/9' }} key={i}>
-          <iframe
-            width="100%"
-            height="100%"
-            src={video}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>,
-      );
-    }
-    setVideos(tmpVideos);
+    setVideosFunc(videos);
     setNextPage(training.next);
     setPrePage(training.pre);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [null, date]);
+
+  // サイズを変えたときに、動画の幅を変える
+  useEffect(() => {
+    const trainings: any = Training;
+    const training: any = date != null ? trainings[date] : {};
+    const videos: Array<any> = training.videos;
+    setVideosFunc(videos);
+// eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isWide])
 
   return (
     <>
@@ -91,22 +121,23 @@ const TrainingMenue = () => {
       <h3 data-testid="breadcrumbs">
         <Link to="/galleryLinks">練習メニュー・風景</Link>&nbsp; &gt; &nbsp; {date}
       </h3>
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableBody>{rows}</TableBody>
-        </Table>
-      </TableContainer>
+      <div style={style}>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableBody>{rows}</TableBody>
+          </Table>
+        </TableContainer>
+      </div>
       {galleryImages.length > 0 && (
-        <div style={style}>
-          <ImageGallery
-            items={galleryImages}
-            showNav={true}
-            autoPlay={true}
-            showFullscreenButton={false}
-            useBrowserFullscreen={false}
-            showPlayButton={true}
-          />
-        </div>
+        <ImageGallery
+          items={galleryImages}
+          showNav={true}
+          autoPlay={true}
+          showFullscreenButton={false}
+          useBrowserFullscreen={false}
+          showPlayButton={true}
+          slideInterval={5000}
+        />
       )}
       {videos}
       <Box component="span" m={1} display="flex" justifyContent="space-between" alignItems="center">
