1a41d01e419cf388b44f79647987ecb3b9f96a4f
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2023-01-02 19:55:25 +0900

    shareボタンの位置調整

--- src/components/parts/ShareButton.tsx
+++ src/components/parts/ShareButton.tsx
@@ -19,27 +19,27 @@ const ShareButton = () => {
   return (
     <>
       <Grid container spacing={0} style={{ paddingTop: '15px' }}>
-        <Grid item xs={2}>
+        <Grid item xs={2} lg={1}>
           <FacebookShareButton url={URL + location.pathname} quote={QUOTE}>
             <FacebookIcon size={48} round />
           </FacebookShareButton>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={2} lg={1}>
           <TwitterShareButton url={URL + location.pathname} title={QUOTE}>
             <TwitterIcon size={48} round />
           </TwitterShareButton>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={2} lg={1}>
           <LineShareButton url={URL + location.pathname} title={QUOTE}>
             <LineIcon size={48} round />
           </LineShareButton>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={2} lg={1}>
           <IconButton
             href="https://github.com/mtaketani113/omu-karate-page"
             style={{ padding: '0px', color: 'black' }}
           >
-            <GitHubIcon sx={{ fontSize: 50 }} />
+            <GitHubIcon sx={{ fontSize: 48 }} />
           </IconButton>
         </Grid>
       </Grid>
