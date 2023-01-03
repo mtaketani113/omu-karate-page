2708f7d80275d2f4607a59dfc19cf3491b8de9b3
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2023-01-04 01:53:07 +0900

    FAQの例を追加

--- src/components/KaratedoFaq.tsx
+++ src/components/KaratedoFaq.tsx
@@ -1,5 +1,23 @@
 const KaratedoFaq = () => {
-  return <>作成中</>;
+  const answerStyle = {
+    paddingLeft: '1em',
+    textIndent: '-1em',
+    paddingBottom: '1em',
+  };
+  return (
+    <>
+      <h4>Q.学業やアルバイトとの両立は可能なのでしょうか？</h4>
+      <div style={answerStyle}>
+        A.学生ですので学業が最優先です。稽古は、現在、週に2・3回、
+        テスト期間中は稽古も休みで、現役部員もアルバイトと両立しています。
+        もちろん、もっと空手をもっとやりたい方は師範、監督、OBでサポートします。
+      </div>
+      <h4>Q.始めるのにどれくらいかかる？</h4>
+      <div style={answerStyle}>
+        A.始めるための道着は共用のものやOB会がサポートしていただけるのでお金はかかりません。
+      </div>
+    </>
+  );
 };
 
 export default KaratedoFaq;
