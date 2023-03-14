0b8df01569abd5f936abd7b24d8c2d02ba4e37a0
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2023-03-15 01:32:30 +0900

    過去のFacebookのリンクを追加

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -18,6 +18,11 @@ const Links = () => {
           大阪市立大学空手道部　＋緑酒会
         </a>
       </li>
+      <li>
+        <a href="https://www.facebook.com/people/%E5%A4%A7%E9%98%AA%E5%BA%9C%E7%AB%8B%E5%A4%A7%E5%AD%A6-%E7%A9%BA%E6%89%8B%E9%81%93%E9%83%A8/100069070396872/">
+          大阪府立大学空手道部
+        </a>
+      </li>
       <h2>合併前のホームページ</h2>
       <li>
         <a href="http://ocukaratedo.web.fc2.com/">大阪市立大学空手道部</a>
