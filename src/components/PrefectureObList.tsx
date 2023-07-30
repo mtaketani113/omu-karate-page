a48c1bf5be3cfe10a66f0c4d450f185abf97b7c0
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2023-07-30 20:48:41 +0900

    keyを追加

--- src/components/PrefectureObList.tsx
+++ src/components/PrefectureObList.tsx
@@ -1,10 +1,4 @@
-import {
-  Table,
-  TableBody,
-  TableHead,
-  TableCell,
-  TableRow,
-} from '@mui/material';
+import { Table, TableBody, TableHead, TableCell, TableRow } from '@mui/material';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import jsonObList from './data/prefectureObList.json';
 import { ColorModeContext, ColorModeContextType } from '../App';
@@ -29,31 +23,31 @@ const PrefectureObList = () => {
     borderColor: colorMode.mode === 'dark' ? 'white' : 'black',
   };
 
-  const graduactionYearKeyList : Array<string> = Object.keys(jsonObList);
+  const graduactionYearKeyList: Array<string> = Object.keys(jsonObList);
   const [obList, setObList] = useState<Array<ReactNode>>([]);
   const [maxColSpan, setMaxColSpan] = useState<number>(1);
 
   useEffect(() => {
     let tmpRow: Array<ReactNode> = [];
     let tmpColSpan: number = 1;
-    for(let i = 0; i < graduactionYearKeyList.length; i ++){
+    for (let i = 0; i < graduactionYearKeyList.length; i++) {
       let tmpCell: Array<ReactNode> = [];
       let thisYearObList: string[] = (jsonObList as any)[graduactionYearKeyList[i]];
       tmpCell.push(
-        <TableCell style={bodyStyle}>{graduactionYearKeyList[i]}</TableCell>
-      )
-      for(let k = 0; k < thisYearObList.length; k++){
+        <TableCell key={graduactionYearKeyList[i]} style={bodyStyle}>
+          {graduactionYearKeyList[i]}
+        </TableCell>,
+      );
+      for (let k = 0; k < thisYearObList.length; k++) {
         tmpCell.push(
-          <TableCell style={bodyStyle}>{thisYearObList[k]}</TableCell>
-        )
+          <TableCell key={thisYearObList[k]} style={bodyStyle}>
+            {thisYearObList[k]}
+          </TableCell>,
+        );
       }
-      tmpRow.push(
-        <TableRow key={graduactionYearKeyList[i]}>
-          {tmpCell}
-        </TableRow>
-      )
-      if(graduactionYearKeyList.length > tmpColSpan){
-        tmpColSpan = graduactionYearKeyList.length
+      tmpRow.push(<TableRow key={graduactionYearKeyList[i]}>{tmpCell}</TableRow>);
+      if (graduactionYearKeyList.length > tmpColSpan) {
+        tmpColSpan = graduactionYearKeyList.length;
       }
     }
     setObList(tmpRow);
@@ -70,21 +64,21 @@ const PrefectureObList = () => {
       </HelmetProvider>
       <h1 data-testid="player">府大OB</h1>
       <Table
-          style={{
-            maxWidth: 500,
-            borderWidth: 1,
-            borderStyle: 'solid',
-            borderColor: colorMode.mode === 'dark' ? 'white' : 'black',
-          }}
-        >
-          <TableHead>
-            <TableRow>
-              <TableCell style={headerStyle}>年度</TableCell>
-              <TableCell style={headerStyle} colSpan={maxColSpan}/>
-            </TableRow>
-          </TableHead>
-          <TableBody>{obList}</TableBody>
-        </Table>
+        style={{
+          maxWidth: 500,
+          borderWidth: 1,
+          borderStyle: 'solid',
+          borderColor: colorMode.mode === 'dark' ? 'white' : 'black',
+        }}
+      >
+        <TableHead>
+          <TableRow>
+            <TableCell style={headerStyle}>年度</TableCell>
+            <TableCell style={headerStyle} colSpan={maxColSpan} />
+          </TableRow>
+        </TableHead>
+        <TableBody>{obList}</TableBody>
+      </Table>
     </>
   );
 };
