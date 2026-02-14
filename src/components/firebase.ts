f5748fd7381fd7cdf63876efb8786172fed4bca6
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2026-02-14 18:43:46 +0900

    youtubeの参照権限を要求するように修正

--- src/components/firebase.ts
+++ src/components/firebase.ts
@@ -14,5 +14,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const googleProvider = new GoogleAuthProvider();
+googleProvider.addScope('https://www.googleapis.com/auth/youtube.readonly')
 
 export { auth, googleProvider };
